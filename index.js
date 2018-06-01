5ac0ffa8f5b7c419d127b693fea2f4c7431e35f5
sofalse <j989898@gmail.com>
2018-06-01 17:05:31 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,134 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const helmet = require('helmet')
+const axios = require('axios')
+const bodyParser = require('body-parser')
+const sqlite3 = require('sqlite3').verbose()
+
+const db = new sqlite3.Database('./database.sqlite')
+const urlEncodedParser = bodyParser.urlencoded({ extended: false })
+
+const SERVER_PORT = 80
+const OMDB_API_KEY = '8640b907'
+const OMDB_LINK = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&t=`
+
+app.use(helmet())
+
+app.post('/movies', urlEncodedParser, (req, res) => {
+    const title = req.body.title
+    if (typeof title === 'undefined' || title.trim() === null || title.trim() === '') {
+        return res.status(400).send('Title is required.')
+    }
+
+    const trimmedTitle = title.trim()
+
+    axios
+        .get(OMDB_LINK + trimmedTitle)
+        .then(omdbRes => {
+            if (omdbRes.data.Response === 'False') {
+                return res.status(404).send('Movie not found')
+            }
+            db.get('SELECT Count(*) AS count FROM movies WHERE imdbID = ?', [omdbRes.data.imdbID], (err, row) => {
+                if (err) throw err
+                if (row.count === 0) {
+                    db.run(
+                        'INSERT INTO movies (Title, Year, Rated, Released, Runtime, Genre, Director, Writer, Actors, Plot, Language, Country, Awards, Metascore, imdbRating, imdbVotes, imdbID, Type, DVD, BoxOffice, Production, Website) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                        [
+                            omdbRes.data.Title.toString(),
+                            omdbRes.data.Year.toString(),
+                            omdbRes.data.Rated.toString(),
+                            omdbRes.data.Released.toString(),
+                            omdbRes.data.Runtime.toString(),
+                            omdbRes.data.Genre.toString(),
+                            omdbRes.data.Director.toString(),
+                            omdbRes.data.Writer.toString(),
+                            omdbRes.data.Actors.toString(),
+                            omdbRes.data.Plot.toString(),
+                            omdbRes.data.Language.toString(),
+                            omdbRes.data.Country.toString(),
+                            omdbRes.data.Awards.toString(),
+                            omdbRes.data.Metascore.toString(),
+                            omdbRes.data.imdbRating.toString(),
+                            omdbRes.data.imdbVotes.toString(),
+                            omdbRes.data.imdbID.toString(),
+                            omdbRes.data.Type.toString(),
+                            omdbRes.data.DVD.toString(),
+                            omdbRes.data.BoxOffice.toString(),
+                            omdbRes.data.Production.toString(),
+                            omdbRes.data.Website.toString(),
+                        ],
+                        err => {
+                            if (err) throw err
+                            console.log(`Movie ${trimmedTitle} added to database.`)
+                        }
+                    )
+                }
+            })
+            res.send(omdbRes.data)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+})
+
+app.get('/movies', (req, res) => {
+    let limit, sort, fields
+    req.query.limit ? (limit = `LIMIT ${req.query.limit}`) : (limit = '')
+    req.query.sort ? (sort = `ORDER BY ${req.query.sort} ASC`) : (sort = '')
+    req.query.fields ? (fields = req.query.fields) : (fields = '')
+    db.all(`SELECT ${fields ? fields : '*'} FROM movies ${sort} ${limit}`, (err, rows) => {
+        if (err) throw err
+        res.send(rows)
+    })
+})
+
+app.post('/comments', urlEncodedParser, (req, res) => {
+    if (
+        req.body.content === null ||
+        req.body.movie === null ||
+        typeof req.body.content === 'undefined' ||
+        typeof req.body.movie === 'undefined'
+    ) {
+        return res.status(400).send('Either content and movie ID are required.')
+    }
+    const trimmedContent = req.body.content.trim()
+    const movieID = req.body.movie.trim()
+    db.get('SELECT Count(*) AS count FROM movies WHERE id = ?', [movieID], (err, row) => {
+        if (row.count === 0) {
+            return res.status(404).send('Movie not found in database.')
+        }
+        db.run('INSERT INTO comments (content, movieID) VALUES (?, ?)', [trimmedContent, movieID], err => {
+            if (err) throw err
+            res.type('json').send(
+                JSON.stringify({
+                    movieID: movieID,
+                    content: trimmedContent,
+                })
+            )
+        })
+    })
+})
+
+app.get('/comments', (req, res) => {
+    let limit, fields, movie
+    req.query.limit ? (limit = `LIMIT ${req.query.limit}`) : (limit = '')
+    req.query.fields ? (fields = req.query.fields) : (fields = '')
+    req.query.movie ? (movie = `WHERE movieID = ${req.query.movie}`) : (movie = '')
+    db.all(`SELECT ${fields ? fields : '*'} FROM comments ${movie} ${limit}`, (err, rows) => {
+        if (err) throw err
+        res.send(rows)
+    })
+})
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Listening on port ${SERVER_PORT}`)
+})
+
+process.on('SIGINT', () => {
+    db.close()
+    process.exit(0)
+})
+
+module.exports = app
---
+++ test/index.test.js
@@ -0,0 +1,234 @@
+let chai = require('chai')
+let expect = chai.expect
+let request = require('supertest')
+let server = require('../index')
+
+describe('GET /movies', () => {
+    it('works with no parameters', done => {
+        request(server)
+            .get('/movies')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, done)
+    })
+    it('works with limit 5', () => {
+        request(server)
+            .get('/movies?limit=5')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .then(res => {
+                const length = res.body.length
+                expect(length).to.be.at.most(5)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    })
+    it('works with limit 0', () => {
+        request(server)
+            .get('/movies?limit=0')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect('Content-Length', '2')
+            .expect(200)
+            .then(res => {
+                expect(res.body.length).to.be.equal(0)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    })
+    it('works with only title field', () => {
+        request(server)
+            .get('/movies?fields=Title')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .then(res => {
+                expect(res.body[0]).to.have.keys('Title')
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    })
+    it('works with only plot field', () => {
+        request(server)
+            .get('/movies?fields=Plot')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .then(res => {
+                expect(res.body[0]).to.have.keys('Plot')
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    })
+    it('works with sorting', () => {
+        request(server)
+            .get('/movies?sort=Year')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .then(res => {
+                expect(parseInt(res.body[0].Year)).to.be.at.most(
+                    parseInt(res.body[1].Year)
+                )
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    })
+    it('does its job with bizarre data', done => {
+        request(server)
+            .get('/movies?hakunamatata=dontworry')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, done)
+    })
+})
+
+describe('POST /movies', () => {
+    it('throws error on no paramaters', () => {
+        request(server)
+            .post('/movies')
+            .expect(400)
+    })
+    it('adds movie to the database', () => {
+        request(server)
+            .post('/movies')
+            .send('title=Jaws')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .then(res => {
+                const body = res.body
+                expect(body.Title).to.equal('Jaws')
+                expect(body.Year).to.equal('1975')
+                expect(body.Rated).to.equal('PG')
+                expect(body.Response).to.equal('True')
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    })
+    it('throws error when movie not found', () => {
+        request(server)
+            .post('/movies')
+            .send('title=asjdofjhuishadfadfiodsi')
+            .expect(404)
+    })
+    it('works fine with strange data', () => {
+        request(server)
+            .post('/movies')
+            .send('title=Jaws')
+            .send('ihearthedrums=echoingtonight')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .then(res => {
+                const body = res.body
+                expect(body.Title).to.equal('Jaws')
+                expect(body.Year).to.equal('1975')
+                expect(body.Rated).to.equal('PG')
+                expect(body.Response).to.equal('True')
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    })
+})
+
+describe('GET /comments', () => {
+    it('works with no parameters', done => {
+        request(server)
+            .get('/comments')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, done)
+    })
+    it('works with limit 5', () => {
+        request(server)
+            .get('/comments?limit=5')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .then(res => {
+                const length = res.body.length
+                expect(length).to.be.at.most(5)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    })
+    it('works with limit 0', () => {
+        request(server)
+            .get('/comments?limit=0')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect('Content-Length', '2')
+            .expect(200)
+            .then(res => {
+                expect(res.body.length).to.be.equal(0)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    })
+    it('works with only content field', () => {
+        request(server)
+            .get('/comments?fields=content')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .then(res => {
+                expect(res.body[0]).to.have.keys('content')
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    })
+    it('works fine with strange data', done => {
+        request(server)
+            .get('/comments?whoareyou=thewinner')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, done)
+    })
+})
+
+describe('POST /comments', () => {
+    it('throws an error while no arguments', () => {
+        request(server)
+            .post('/comments')
+            .expect(400)
+    })
+    it('throws an error while no movie ID', () => {
+        request(server)
+            .post('/comments')
+            .send('content=Bizzaire!')
+            .expect(400)
+    })
+    it('throws an error while no content', () => {
+        request(server)
+            .post('/comments')
+            .send('movie=2')
+            .expect(400)
+    })
+    it('works fine when it should to', () => {
+        request(server)
+            .post('/comments')
+            .send('movie=2')
+            .send('content=Bizzare!')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .then(res => {
+                expect(res.body).to.have.keys('movieID', 'content')
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    })
+    it('works fine with strange data', () => {
+        request(server)
+            .post('/comments')
+            .send('movie=2')
+            .send('content=Bizzare!')
+            .send('shouldyouhireme=yesplz')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200)
+            .then(res => {
+                expect(res.body).to.have.keys('movieID', 'content')
+            })
+            .catch(err => {
+                console.error(err)
+            })
+    })
+})
