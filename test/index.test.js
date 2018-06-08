44af92218e111cce4679109317d844730a015ce3
sofalse <j989898@gmail.com>
2018-06-08 16:22:10 +0200

    Fixed unit tests

--- test/index.test.js
+++ test/index.test.js
@@ -11,7 +11,7 @@ describe('GET /movies', () => {
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(200, done)
     })
-    it('works with limit 5', () => {
+    it('works with limit 5', done => {
         request(server)
             .get('/movies?limit=5')
             .expect('Content-Type', 'application/json; charset=utf-8')
@@ -19,12 +19,13 @@ describe('GET /movies', () => {
             .then(res => {
                 const length = res.body.length
                 expect(length).to.be.at.most(5)
+                done()
             })
             .catch(err => {
                 console.error(err)
             })
     })
-    it('works with limit 0', () => {
+    it('works with limit 0', done => {
         request(server)
             .get('/movies?limit=0')
             .expect('Content-Type', 'application/json; charset=utf-8')
@@ -32,6 +33,7 @@ describe('GET /movies', () => {
             .expect(200)
             .then(res => {
                 expect(res.body.length).to.be.equal(0)
+                done()
             })
             .catch(err => {
                 console.error(err)
@@ -43,25 +45,27 @@ describe('GET /movies', () => {
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(200, done)
     })
-    it('works with only title field', () => {
+    it('works with only title field', done => {
         request(server)
             .get('/movies?fields=Title')
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(200)
             .then(res => {
                 expect(res.body[0]).to.have.keys('Title')
+                done()
             })
             .catch(err => {
                 console.error(err)
             })
     })
-    it('works with only plot field', () => {
+    it('works with only plot field', done => {
         request(server)
             .get('/movies?fields=Plot')
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(200)
             .then(res => {
                 expect(res.body[0]).to.have.keys('Plot')
+                done()
             })
             .catch(err => {
                 console.error(err)
@@ -73,13 +77,14 @@ describe('GET /movies', () => {
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(200, done)
     })
-    it('works with sorting', () => {
+    it('works with sorting', done => {
         request(server)
             .get('/movies?sort=Year')
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(200)
             .then(res => {
                 expect(parseInt(res.body[0].Year)).to.be.at.most(parseInt(res.body[1].Year))
+                done()
             })
             .catch(err => {
                 console.error(err)
@@ -94,12 +99,12 @@ describe('GET /movies', () => {
 })
 
 describe('POST /movies', () => {
-    it('throws error on no paramaters', () => {
+    it('throws error on no paramaters', done => {
         request(server)
             .post('/movies')
-            .expect(400)
+            .expect(400, done)
     })
-    it('adds movie to the database', () => {
+    it('adds movie to the database', done => {
         request(server)
             .post('/movies')
             .send('title=Jaws')
@@ -111,18 +116,19 @@ describe('POST /movies', () => {
                 expect(body.Year).to.equal('1975')
                 expect(body.Rated).to.equal('PG')
                 expect(body.Response).to.equal('True')
+                done()
             })
             .catch(err => {
                 console.error(err)
             })
     })
-    it('throws error when movie not found', () => {
+    it('throws error when movie not found', done => {
         request(server)
             .post('/movies')
             .send('title=asjdofjhuishadfadfiodsi')
-            .expect(404)
+            .expect(404, done)
     })
-    it('works fine with strange data', () => {
+    it('works fine with strange data', done => {
         request(server)
             .post('/movies')
             .send('title=Jaws')
@@ -135,6 +141,7 @@ describe('POST /movies', () => {
                 expect(body.Year).to.equal('1975')
                 expect(body.Rated).to.equal('PG')
                 expect(body.Response).to.equal('True')
+                done()
             })
             .catch(err => {
                 console.error(err)
@@ -149,7 +156,7 @@ describe('GET /comments', () => {
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(200, done)
     })
-    it('works with limit 5', () => {
+    it('works with limit 5', done => {
         request(server)
             .get('/comments?limit=5')
             .expect('Content-Type', 'application/json; charset=utf-8')
@@ -157,12 +164,13 @@ describe('GET /comments', () => {
             .then(res => {
                 const length = res.body.length
                 expect(length).to.be.at.most(5)
+                done()
             })
             .catch(err => {
                 console.error(err)
             })
     })
-    it('works with limit 0', () => {
+    it('works with limit 0', done => {
         request(server)
             .get('/comments?limit=0')
             .expect('Content-Type', 'application/json; charset=utf-8')
@@ -170,6 +178,7 @@ describe('GET /comments', () => {
             .expect(200)
             .then(res => {
                 expect(res.body.length).to.be.equal(0)
+                done()
             })
             .catch(err => {
                 console.error(err)
@@ -181,13 +190,14 @@ describe('GET /comments', () => {
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(200, done)
     })
-    it('works with only content field', () => {
+    it('works with only content field', done => {
         request(server)
             .get('/comments?fields=content')
             .expect('Content-Type', 'application/json; charset=utf-8')
             .expect(200)
             .then(res => {
                 expect(res.body[0]).to.have.keys('content')
+                done()
             })
             .catch(err => {
                 console.error(err)
@@ -208,30 +218,31 @@ describe('GET /comments', () => {
 })
 
 describe('POST /comments', () => {
-    it('throws an error while no arguments', () => {
+    it('throws an error while no arguments', done => {
         request(server)
             .post('/comments')
-            .expect(400)
+            .expect(400, done)
     })
-    it('throws an error while no movie ID', () => {
+    it('throws an error while no movie ID', done => {
         request(server)
             .post('/comments')
             .send('content=Bizzaire!')
-            .expect(400)
+            .expect(400, done)
     })
-    it('throws an error while no content', () => {
+    it('throws an error while no content', done => {
         request(server)
             .post('/comments')
             .send('movie=2')
-            .expect(400)
+            .expect(400, done)
     })
-    it('throws 404 when no movie found', () => {
+    it('throws 404 when no movie found', done => {
         request(server)
             .post('/comments')
+            .send('content=Obnoxious!')
             .send('movie=777')
-            .expect(404)
+            .expect(404, done)
     })
-    it('works fine when it should to', () => {
+    it('works fine when it should to', done => {
         request(server)
             .post('/comments')
             .send('movie=2')
@@ -240,12 +251,13 @@ describe('POST /comments', () => {
             .expect(201)
             .then(res => {
                 expect(res.body).to.have.keys('movieID', 'content')
+                done()
             })
             .catch(err => {
                 console.error(err)
             })
     })
-    it('works fine with strange data', () => {
+    it('works fine with strange data', done => {
         request(server)
             .post('/comments')
             .send('movie=2')
@@ -255,6 +267,7 @@ describe('POST /comments', () => {
             .expect(201)
             .then(res => {
                 expect(res.body).to.have.keys('movieID', 'content')
+                done()
             })
             .catch(err => {
                 console.error(err)
