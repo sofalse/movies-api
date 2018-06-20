2ac8e7cacddec350718e6bec606d515f6290be2b
sofalse <j989898@gmail.com>
2018-06-20 11:43:41 +0200

    Comment model update for future population

--- src/models/Comment.ts
+++ src/models/Comment.ts
@@ -2,7 +2,7 @@ import { model, Schema } from 'mongoose'
 
 const Comment = new Schema({
     content: String,
-    movieID: String,
+    movieID: { type: Schema.Types.ObjectId, ref: 'Movie' },
 }, { timestamps: true })
 
 export default model('Comment', Comment)
