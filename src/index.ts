e93b300d03f998dd4a96874ebfb7e5ecce293026
sofalse <j989898@gmail.com>
2018-06-22 23:39:47 +0200

    Added swagger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import bodyParser from 'body-parser'
 import compression from 'compression'
+import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import expressValidator from 'express-validator'
 import helmet from 'helmet'
+// @ts-ignore
+import swaggerUI from 'swagger-ui-express'
 import router from './routes/routes'
 import Database from './utils/database'
 
@@ -17,6 +20,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(helmet())
 app.use(expressValidator())
 app.use(compression())
+app.use(cors())
+// tslint:disable-next-line:no-var-requires
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(require('../swagger.json')))
 
 app.use('/', router)
 
